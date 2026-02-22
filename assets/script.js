e1343e1288b88fd96de6535da1910fc290d097bf
Md Sharif Foysal Shoron <mdshariffoysalshoron@gmail.com>
2026-02-22 16:17:14 +0600

    Add initial implementation of Matrix rain effect and video background

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById('Matrix');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const context = canvas.getContext('2d');
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const nums = '0123456789০১২৩৪৫৬৭৮৯';
+const bangla = 'অআইঈউঊঋএঐওঔকখগঘঙচছজঝঞটঠডঢণতথদধনপফবভমযরলশষসহড়ঢ়য়ৎ ং ঃ ঁ';
+const arabic = 'ابتثجحخدذرزسشصضطظعغفقكلمنهـوي';
+const alphabet = arabic + katakana + bangla + latin + nums;
+const fontSize = 22;
+const columns = canvas.width / fontSize;
+const rainDrops = [];
+for (let x = 0; x < columns; x++) {
+    rainDrops[x] = 1;
+}
+const draw = () => {
+    context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = '#0F0';
+    context.font = fontSize + 'px monospace';
+    for (let i = 0; i < rainDrops.length; i++) {
+        const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
+        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+            rainDrops[i] = 0;
+        }
+        rainDrops[i]++;
+    }
+};
+setInterval(draw, 30);
